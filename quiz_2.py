0ab79640b755b566413e751f967493c47f949278
solki <solkic@gmail.com>
2016-03-03 18:08:02 +1100

    finished quiz2

--- quiz_2.py
+++ quiz_2.py
@@ -22,24 +22,26 @@ try:
 except ValueError:
     print('Incorrect input, giving up.')
     sys.exit()
-        
+
+
 def display_encoded_set(encoded_set):
-    encoded_set = bin(encoded_set)[2 :][: : -1]
-    print('{', end = '')
+    encoded_set = bin(encoded_set)[2:][:: -1]
+    print('{', end='')
     found_bit = False
     for i in range(len(encoded_set) // 2 * 2 - 1, -1, -2):
         if encoded_set[i] == '1':
             if found_bit:
-                print(', ', end = '')
-            print(-(i + 1) // 2, end = '')
+                print(', ', end='')
+            print(-(i + 1) // 2, end='')
             found_bit = True
-    for i in range(0, len(encoded_set) , 2):
+    for i in range(0, len(encoded_set), 2):
         if encoded_set[i] == '1':
             if found_bit:
-                print(', ', end = '')
-            print(i // 2, end = '')
+                print(', ', end='')
+            print(i // 2, end='')
             found_bit = True
     print('}')
+
 
 def code_derived_set(encoded_set):
     encoded_running_sum = 0
@@ -53,14 +55,26 @@ def code_derived_set(encoded_set):
     # appended to running_sums
     running_sum = 0
     # As done in display_encoded_set()...
-    encoded_set = bin(encoded_set)[2 :][: : -1]
+    encoded_set = bin(encoded_set)[2:][:: -1]
     # REPLACE THIS COMMENT WITH YOUR CODE
+    for i in range(len(encoded_set) // 2 * 2 - 1, -1, -2):
+        if encoded_set[i] == '1':
+            running_sum += -(i + 1) // 2
+            running_sums.append(running_sum)
+    for i in range(0, len(encoded_set), 2):
+        if encoded_set[i] == '1':
+            running_sum += i // 2
+            running_sums.append(running_sum)
+    for num in set(running_sums):
+        if num < 0:
+            encoded_running_sum += 2**(-num*2-1)
+        else:
+            encoded_running_sum += 2**(num*2)
     return encoded_running_sum
 
-print('The encoded set is: ', end = '')
-display_encoded_set(encoded_set)
-    
-print('The derived encoded set is: ', end = '')
-display_encoded_set(code_derived_set(encoded_set))
 
-    
+print('The encoded set is: ', end='')
+display_encoded_set(encoded_set)
+
+print('The derived encoded set is: ', end='')
+display_encoded_set(code_derived_set(encoded_set))
